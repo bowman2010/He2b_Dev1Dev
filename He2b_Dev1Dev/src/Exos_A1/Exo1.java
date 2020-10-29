75bc623e2dc16d9130617145f1b4e3d3b9bedc87
bowman <bowman@phobos>
2020-10-29 22:13:14 +0100

    First publication on GitHub

---
+++ He2b_Dev1Dev/src/Exos_A1/Exo1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exos_A1;
+
+import FvpLib.Utils;
+
+/**
+ * Somme de deux nombres;
+ * @author bowman
+ */
+public class Exo1 {
+    
+    public static void main(String[] args) {
+        double a = Utils.askForDouble("Entrez nombre a : ");
+        double b = Utils.askForDouble("Entrez nombre b : ");
+        Utils.println(""+a+"+"+b+"="+(a+b));
+    }
+    
+}
