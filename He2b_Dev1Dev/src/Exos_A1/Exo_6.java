6f58854fbabe20d8b17b6f11503351e791d24d18
bowman <bowman@phobos>
2020-10-30 13:43:45 +0100

    First publication on GitHub

--- He2b_Dev1Dev/src/Exos_A1/Exo_6.java
+++ He2b_Dev1Dev/src/Exos_A1/Exo_6.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Exos_A1;
 
 import FvpLib.Utils;
@@ -15,21 +11,25 @@ public class Exo_6 {
     
     
     public static void main(String[] args) {
+        Utils.horizLn(60, '=');
+        Utils.println("Calcul de TVA");
+        Utils.horizLn(60, '=');
+        
 
         double htva  = Utils.askForDouble("Prix HTVA : ");
-        double tva   = Utils.askForDouble("      TVA ;  ");
+        double tva   = Utils.askForDouble("      TVA ; ");
 
         
-        var htvastr =  Utils.dotAligned( 7, 2, htva);
+        String htvastr =  Utils.dotAligned( 7, 2, htva);
         String tvastr  =  Utils.dotAligned( 7, 2, tva);
-        String ttcstr  =  Utils.dotAligned( 7, 2, fve);
-          
+        Utils.horizLn(60, '-');
+        String ttcstr  =  Utils.dotAligned( 7, 2, htva+tva);
+        Utils.horizLn(60, '-');
         System.out.println ("Prix HTVA = "+ htvastr);
         System.out.println ("TVA       = "+ tvastr);
         System.out.println ("Prix TTC  = "+ ttcstr);                                 
+        Utils.horizLn(60, '=');
 
-                
-        
     }
     
 }
